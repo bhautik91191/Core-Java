fade6068b28d717f53257885143e90566d723d91
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-03 19:42:04 +0530

    Update AgeCheck.java

--- AgeCheck.java
+++ AgeCheck.java
@@ -4,10 +4,10 @@ package com.corejava;
 public class AgeCheck {
 
 	public static void main(String[] args) {
-		int age = 30; // Age is a variable with value 30 
+		int ageSize = 30; // Age is a variable with value 30 
 		
 		// If / else condition use
-		if (age < 25) {
+		if (ageSize < 25) {
 			System.out.println("Your Age is 25 Down");
 		} 
 		else {
