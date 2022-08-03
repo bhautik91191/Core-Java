e8cfffa32b5ee5ebb6767fa7ad056f2082a64abe
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-03 15:01:28 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.corejava;
+/*
+Write a program to print “Hello world”.
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
