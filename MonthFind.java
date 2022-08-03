1d697eca3c6f01f4e5e671ad08df18042a7115f6
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-03 19:48:54 +0530

    Update MonthFind.java

--- MonthFind.java
+++ MonthFind.java
@@ -1,52 +1,52 @@
 package com.corejava;
 
-// documentation to if else condition
+// Documentation to if else condition
 public class MonthFind {
 
 	public static void main(String[] args) {
-		int month = 5;
+		int monthLength = 5;
 		
 		/* 
-		  uses switch case statement 
+		  Uses switch case statement 
 		  and find current month.
 		*/
-		switch (month) {
-		case 1:
-			System.out.println("current month is January");
-			break;
-		case 2:
-			System.out.println("current month is February");
-			break;
-		case 3:
-			System.out.println("current month is March");
-			break;
-		case 4:
-			System.out.println("current month is April");
-			break;
-		case 5:
-			System.out.println("current month is May");
-			break;
-		case 6:
-			System.out.println("current month is June");
-			break;
-		case 7:
-			System.out.println("current month is July");
-			break;
-		case 8:
-			System.out.println("current month is August");
-			break;
-		case 9:
-			System.out.println("current month is September");
-			break;
-		case 10:
-			System.out.println("current month is October");
-			break;
-		case 11:
-			System.out.println("current month is November");
-			break;
-		case 12:
-			System.out.println("current month is December");
-			break;
+		switch (monthLength) {
+		      	case 1:
+				System.out.println("current month is January");
+				break;
+			case 2:
+				System.out.println("current month is February");
+				break;
+			case 3:
+				System.out.println("current month is March");
+				break;
+			case 4:
+				System.out.println("current month is April");
+				break;
+			case 5:
+				System.out.println("current month is May");
+				break;
+			case 6:
+				System.out.println("current month is June");
+				break;
+			case 7:
+				System.out.println("current month is July");
+				break;
+			case 8:
+				System.out.println("current month is August");
+				break;
+			case 9:
+				System.out.println("current month is September");
+				break;
+			case 10:
+				System.out.println("current month is October");
+				break;
+			case 11:
+				System.out.println("current month is November");
+				break;
+			case 12:
+				System.out.println("current month is December");
+				break;
 		}		
 	}
 
