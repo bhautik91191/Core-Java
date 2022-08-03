6f7d81d9073646eef2b43845496e44d488198834
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-03 20:25:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
 package com.corejava;
 
-// documentation to print “Hello world”.
+/**
+* The HelloWorld program implements an application that
+* Simply displays "Hello World!" to the standard output.
+*
+* @author  Zara Ali
+* @version 1.0
+* @since   2014-03-31 
+*/
 public class HelloWorld {
 
 	public static void main(String[] args) {
