3503805e1dc1b396d0ae617fc5f7f7739152a50b
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-03 19:44:26 +0530

    Update BreakAndContinue.java

--- BreakAndContinue.java
+++ BreakAndContinue.java
@@ -6,20 +6,17 @@ public class BreakAndContinue {
 		System.out.println("Print 1 To 10 Num But After 7 Num to Break Statment");
 		
 		for (int i = 1; i <= 10; i++) {
-			if (i == 7) 
-			{
-				break; // uses break statment
+			if (i == 7) {
+				break; // Uses break statment
 			}
 			System.out.println(i);
-		}
-		
-		System.out.println(); // space after program
+		}		
+		System.out.println(); // Space after program
 		System.out.println("Print 1 To 10 Num 7 Num Skip And Continue Loop");
 		
 		for (int j = 1; j <= 10; j++) {
-			if (j == 7) 
-			{
-				continue; // uses continue statment
+			if (j == 7) {
+				continue; // Uses continue statment
 			}
 			System.out.println(j);
 		}
