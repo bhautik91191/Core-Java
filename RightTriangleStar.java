ebd049398bdcb56aa9caa14de47990f63fd3f976
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-04 11:31:49 +0530

    Update RightTriangleStar.java

--- RightTriangleStar.java
+++ RightTriangleStar.java
@@ -1,24 +1,32 @@
 package com.corejava;
 
-/*
- Right triangle star output :
- * 
- * * 
- * * * 
- * * * * 
- * * * * *
+/**
+* Uses for loop and print the below-mentioned pattern.
+* Right Triangle Star Output :
+  * 
+  * * 
+  * * * 
+  * * * * 
+  * * * * *
+*
+* @author  Bhautik Padmani
+* @version 1.8
+* @since   March 2014 
 */
 public class RightTriangleStar {
 
 	public static void main(String[] args) {
-		int n = 5;
+		int rowNumber = 5;
 		int i, j;
-		
-		for(i = 0; i < n; i++) {  // Outer loop for number of rows(n)
-			for(j = 0; j <= i; j++) { // Inner loop for column       
+
+		// Outer loop for number of rows(n) 
+		for(i = 0; i < rowNumber; i++) {
+
+			// Inner loop for columns
+			for(j = 0; j <= i; j++) {   
 			    System.out.print("* "); // Print star
-			}           
-        	   System.out.println(); // Ending line after each row
+			}
+              	    System.out.println(); // Ending line after each row
 		}
 	}
 
