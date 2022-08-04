c26cbeb59066bb8b6fd616fb247e0db95848fdc3
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-04 18:18:02 +0530

    Add files via upload

---
+++ StaticDemo.java
@@ -0,0 +1,60 @@
+package com.corejava;
+
+/**
+* Static Keyword uses static methods, variables, and blocks
+* Static method to use static variable and store value to output method variable value
+* Static variable last value set memory store
+* Static block buy default run with class not static block object
+*
+* @author  Bhautik Padmani
+* @version 1.8
+* @since   March 2014 
+*/
+class StaticKeywordDemo{
+
+	int number;
+	static int staticNumber; // Static Variable
+
+	// Static Method
+	public static void show() {
+		staticNumber = 123456;
+		System.out.println("Static Method Number is ..." + staticNumber);
+	}
+
+	// Static Block
+	static {
+		staticNumber = 100;
+		System.out.println("Static block is..." + staticNumber);
+	}
+
+}
+
+public class StaticDemo {
+
+	public static void main(String[] args) {
+		StaticKeywordDemo demo1 = new StaticKeywordDemo();
+		StaticKeywordDemo demo2 = new StaticKeywordDemo();
+		StaticKeywordDemo demo3 = new StaticKeywordDemo();
+		demo1.show();
+
+		// Local variable set values
+		demo1.number = 10;
+		demo2.number = 20;
+		demo3.number = 30;
+
+		System.out.println("First Number is " + demo1.number);
+		System.out.println("Second Number is " + demo2.number);
+		System.out.println("Third Number is " + demo3.number);
+
+		// Static variable set value
+		demo1.staticNumber = 10;
+		demo2.staticNumber = 20;
+		demo3.staticNumber = 30;
+
+		// Stitic varible last value store output print
+		System.out.println("First Static Number is " + demo1.staticNumber);
+		System.out.println("Second Static Number is " + demo2.staticNumber);
+		System.out.println("Third Static Number is " + demo3.staticNumber);
+	}
+
+}
---
+++ ThisOrSuper_Demo.java
@@ -0,0 +1,59 @@
+package com.corejava;
+
+/**
+* This Demo class two variable same name parameterized or local and uses this keyword
+* Two class parent and child class and extands to child to parent
+* parent local variale use to super keyword
+*
+* @author  Bhautik Padmani
+* @version 1.8
+* @since   March 2014 
+*/
+class ThisDemo {
+
+	String name; // Local variable
+	int age;
+
+	public void setData(String name, int age) {
+		this.name = name; // Use this Same name variable parameterized variable or local variable
+		this.age = age;
+	}
+
+	public void show() {
+		System.out.println("My Name is " + name);
+		System.out.println("My Age is " + age);
+	}
+
+}
+
+// Parent class
+class SuperDemo1 {
+
+	String colour = "Black"; // Same name variable
+
+}
+
+// Child class to extends parent class
+class SuperDemo2 extends SuperDemo1 {
+
+	String colour = "White"; // Same name variable
+
+	public void showColo() {
+		System.out.println("My Favorite Colour.." + colour); // Prints color of SuperDemo2 class
+		System.out.println("My Favorite Colour.." + super.colour); // Prints color of SuperDemo1 class
+	}
+
+}
+
+public class ThisOrSuper_Demo {
+
+	public static void main(String[] args) {
+		ThisDemo thisDemo = new ThisDemo();
+		thisDemo.setData("Bhautik", 22);
+		thisDemo.show();
+
+		SuperDemo2 superDemo = new SuperDemo2();
+		superDemo.showColo();
+	}
+
+}
