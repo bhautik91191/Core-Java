4bf1dd0a13c47b63a59904819ce864e61998ed6a
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-04 11:16:52 +0530

    Update PrintUsingWhile.java

--- PrintUsingWhile.java
+++ PrintUsingWhile.java
@@ -1,27 +1,33 @@
 package com.corejava;
 
-// Documentation to while loop and do while loop
+/**
+* Print 1 to 10 numbers
+* Uses While Loop and Do While Loop
+*
+* @author  Bhautik Padmani
+* @version 1.8
+* @since   March 2014 
+*/
 public class PrintUsingWhile {
 
 	public static void main(String[] args) {
-		int number1 = 1; 
-		
-		// Print statement
+		int number1 = 1; // Loop counter initialisation
+
 		System.out.println("While Loop Use Output is : ");
-		
+
+		// Loop to print 1 to 10.
 		while (number1 <= 10) {
 			System.out.println(number1);
 			number1++;
-		}	
+		}
 		int number2 = 1;
-		
-		// Print statement
+
 		System.out.println("Do While Loop Use Output is : ");
-				
+
 		do {
 			System.out.println(number2);
 			number2++;
-		} while (number2 <= 10); 
+		} while (number2 <= 10);
 	}
 
 }
