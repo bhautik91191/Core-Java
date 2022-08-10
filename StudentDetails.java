b15eb38b7ea2cf3a14b0ff903d895b42ef57f28a
bhautik padmani <110401869+bhautik91191@users.noreply.github.com>
2022-08-10 18:37:52 +0530

    Update StudentDetails.java

--- StudentDetails.java
+++ StudentDetails.java
@@ -25,9 +25,9 @@ public class StudentDetails {
 	int age;
 
 	StudentDetails(int rollno, String name, int age) {
-		this.rollno=rollno;
-		this.name=name;
-		this.age=age;
+		this.rollno = rollno;
+		this.name = name;
+		this.age = age;
 	}
 
 }
